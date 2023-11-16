55f82426ac14b97daa5b1138884321ffa6758ac1
Max <max.nussbaumer@tum.de>
2023-11-16 17:27:32 +0100

    update

--- src/main/java/com/example/plagcheck/controller/ComparisonController.java
+++ src/main/java/com/example/plagcheck/controller/ComparisonController.java
@@ -1,15 +1,10 @@
 package com.example.plagcheck.controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.stream.Collectors;
-import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 import org.json.JSONException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +21,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.plagcheck.storage.FileSystemStorageService;
 import com.example.plagcheck.storage.StorageFileNotFoundException;
 import com.example.plagcheck.storage.StorageService;
 
@@ -36,7 +30,6 @@ public class ComparisonController {
 
 	private final StorageService storageService;
 
-	@Autowired
 	public ComparisonController(StorageService storageService) {
 		this.storageService = storageService;
 	}
